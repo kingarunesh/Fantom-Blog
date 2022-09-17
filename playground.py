8985a56ff9b7ffc88aaf905f2ee65565e1556b51
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-09-17 21:04:35 +0530

    3.2 : New post save in database

---
+++ playground.py
@@ -0,0 +1,6 @@
+from datetime import datetime as dt
+
+
+now = dt.now().strftime("%d %B %Y")
+
+print(now)
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from importlib.resources import path
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime as dt
 
 
 app = Flask(__name__)
@@ -11,8 +11,8 @@ app = Flask(__name__)
 #           DATABASE
 #
 ##################################################################################
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blogs.db'
+db = SQLAlchemy(app)
 
 
 # class User(db.Model):
@@ -24,16 +24,20 @@ app = Flask(__name__)
 #     admin = db.Column(db.Boolean, default=False, nullable=False)
 
 
-# class Post(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(300), unique=True, nullable=False)
-#     description = db.Column(db.Text, nullable=False)
-#     image_url = db.Column(db.String(500), nullable=False)
-#     category = db.Column(db.String(100), nullable=False)
-#     total_view = db.Column(db.Integer, nullable=False)
-#     created_date = db.Column(db.String(250), nullable=False)
-#     updated_date = db.Column(db.String(250), nullable=False)
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(500), unique=True, nullable=False)
+    subtitle = db.Column(db.String(500), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.String(500), nullable=False)
+    category = db.Column(db.String(250), nullable=False)
+    tags = db.Column(db.String(500), nullable=False)
+    total_view = db.Column(db.Integer, nullable=False)
+    created_date = db.Column(db.String(250), nullable=False)
+    updated_date = db.Column(db.String(250), nullable=False)
 
+
+db.create_all()
 
 ##################################################################################
 #
@@ -45,8 +49,37 @@ def dashboard():
     return render_template("admin/index.html", path=request.path)
 
 
-@app.route("/admin/new-post")
+@app.route("/admin/new-post", methods=["GET", "POST"])
 def new_post():
+
+    if request.method == "POST":
+        title = request.form["title"]
+        subtitle = request.form["subtitle"]
+        description = request.form["description"]
+        image_url = request.form["image_url"]
+        category = request.form["category"]
+        tags = request.form["tags"]
+        created_date = dt.now().strftime("%d %B %Y")
+        updated_date = dt.now().strftime("%d %B %Y")
+        total_view = 1
+
+        add_post = Post(
+            title=title,
+            subtitle=subtitle,
+            description=description,
+            image_url=image_url, 
+            category=category, 
+            tags=tags,
+            created_date=created_date,
+            updated_date=updated_date,
+            total_view=total_view
+            )
+
+        db.session.add(add_post)
+        db.session.commit()
+
+        return redirect(url_for('get_all_post'))
+
     return render_template("admin/new-post.html", path=request.path)
 
 
