cd37802a5bc28455b4e311fd7a7621b0a3c80282
Arunesh kumar <aruneshkumar.king@gmail.com>
2022-09-26 02:06:27 +0530

    23.8 : Custom error page added

--- main.py
+++ main.py
@@ -20,6 +20,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY']='iloveyounidawhyyoublockedme'
 
 
+#   custom error page added
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('errors/404.html'), 404
+
+
+def page_not_found(e):
+  return render_template('404.html'), 404
+
+
 #   CKEditor
 ckeditor = CKEditor(app)
 
